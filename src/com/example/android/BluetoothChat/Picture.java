0d0e1441577e56e8a03c2c03206beca3d9a8882c
ktotutktit1463 <ktotutktit1463@gmail.com>
2014-11-20 22:29:30 +0900

    commite

--- src/com/example/android/BluetoothChat/Picture.java
+++ src/com/example/android/BluetoothChat/Picture.java
@@ -2,17 +2,12 @@ package com.example.android.BluetoothChat;
 
 import java.io.Serializable;
 
-import android.app.Activity;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-
 public class Picture implements Serializable {
 	/**
-	 * 
+	 *
 	 */
 	public byte[] b;//画像とバイトデータを格納する
 	public String touchEV = "touchEvent";//タッチされてた座標値をメッセージとして送る
 	public int flag = 0;//画像を送るのか、座標値を送るのかのフラグ
-	
+
 }
